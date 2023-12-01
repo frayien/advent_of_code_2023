9fc19c44ec9ccdf4abd49ee210f23e566011b6ef
Adrien <garbani.adrien@yahoo.fr>
2023-12-01 22:35:10 +0100

    1.2

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, collections::{HashMap, BTreeMap}};
 
 pub fn day1_1(path: &str) -> i32 {
     let content = fs::read_to_string(path).unwrap();
@@ -22,6 +22,76 @@ pub fn day1_1(path: &str) -> i32 {
                 break;
             }
         }
+
+        total += word.parse::<i32>().unwrap();
+    }
+
+    total
+}
+
+pub fn day1_2(path: &str) -> i32 {
+    let content = fs::read_to_string(path).unwrap();
+
+    let mut values = HashMap::new();
+    values.insert(String::from("one"), 1);
+    values.insert(String::from("two"), 2);
+    values.insert(String::from("three"), 3);
+    values.insert(String::from("four"), 4);
+    values.insert(String::from("five"), 5);
+    values.insert(String::from("six"), 6);
+    values.insert(String::from("seven"), 7);
+    values.insert(String::from("eight"), 8);
+    values.insert(String::from("nine"), 9);
+
+    let mut total = 0;
+
+    for line in content.lines() {
+
+        let mut word: String = "".to_string();
+
+        let mut vals = BTreeMap::new();
+
+        for (k, v) in values.iter() {
+            if let Some(id) = line.find(k) {
+                vals.insert(id, v);
+            }
+        }
+
+        for (k, v) in values.iter() {
+            if let Some(id) = line.rfind(k) {
+                vals.insert(id, v);
+            }
+        }
+
+        let first = vals.iter().next();
+        let last = vals.iter().next_back();
+
+        for (i, c) in line.chars().enumerate() {
+            if first.is_some_and(|(k,_)| k < &i) {
+                word.push(char::from_digit(**first.unwrap().1, 10).unwrap());
+                break;
+            }
+            
+            if c.is_ascii_digit() {
+                word.push(c);
+                break;
+            }
+        }
+
+        for (i, c) in line.chars().rev().enumerate() {
+            let ri = line.len()-1 - i;
+            if last.is_some_and(|(k,_)| k > &ri) {
+                word.push(char::from_digit(**last.unwrap().1, 10).unwrap());
+                break;
+            }
+
+            if c.is_ascii_digit() {
+                word.push(c);
+                break;
+            }
+        }
+
+        println!("{} {}", line, word);
 
         total += word.parse::<i32>().unwrap();
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 
 mod day1;
 
-use day1::day1_1;
+use day1::day1_2;
 
 fn main() {
     let path = "day1.txt";
-    let res = day1_1(path);
+    let res = day1_2(path);
 
     println!("res : {}", res);
 }
